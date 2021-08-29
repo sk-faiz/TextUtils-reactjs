6e2c4b4958bef620cf0b0e1888040ee4276ab9f0
sk-faiz <shaikh.faiz2202@gmail.com>
2021-08-29 20:25:06 +0530

    First commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+
+function navbar(props) {
+    return (
+        <div>
+         
+            <nav className={`navbar navbar-expand-lg  navbar-${props.mode} bg-${props.mode}`}>
+              <div className="container-fluid">
+                <a className="navbar-brand" href="/">{props.title}</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                      <Link to="/" className="nav-link active" aria-current="page">Home</Link>
+                    </li>
+                    
+                      <li className="nav-item">
+                      <Link to='/about' className="nav-link">{`About`}</Link>
+                      </li>
+                    
+                  </ul>
+                </div>
+                <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+                  <input className="form-check-input" onClick={props.button} type="checkbox" id="flexSwitchCheckDefault"/>
+                  <label className={`form-check-label`} htmlFor="flexSwitchCheckDefault">{`${props.mode==='light'?'Dark':'Light'}`} Mode</label>
+                </div>
+              </div>
+           </nav>
+          
+        </div>
+    )
+}
+
+export default navbar
